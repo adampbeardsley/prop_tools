9fa3a5b46febb1ff6ee93da84c2dea20f3f5617e
Adam Beardsley <adam.p.beardsley@gmail.com>
2017-09-13 15:21:07 -0700

    Adding comments

--- timeline_example.py
+++ timeline_example.py
@@ -2,6 +2,7 @@ import timeline
 
 example = timeline.Timeline()
 
+# Create tasks with start date, end date, description, and category.
 example.add_task('1 May 2018', '15 April 2019', '21cmFAST runs', 'red')
 example.add_task('1 Jan 2019', '1 April 2020', 'Mock 21cm Observations', 'red')
 example.add_task('1 July 2019', '1 Dec 2020', 'zre PDFs', 'red')
@@ -14,6 +15,7 @@ example.add_task('1 Jan 2019', '15 Oct 2019', 'initial mapping function', 'blue'
 example.add_task('1 April 2019', '15 March 2021', 'joint 21 cm field galaxy catalogs', 'yellow')
 example.add_task('1 Oct 2019', '1 July 2020', 'preliminary forecasts', 'yellow')
 
+# Create events with date, description, and category.
 example.add_event('1 Oct 2018', 'Conf 1', 'Conf')
 example.add_event('1 June 2019', 'Conf 2', 'Conf')
 example.add_event('1 March 2020', 'Conf 3', 'Conf')
@@ -21,4 +23,5 @@ example.add_event('29 Sep 2019', 'Paper 1', 'Paper')
 example.add_event('20 May 2020', 'Paper 2', 'Paper')
 example.add_event('1 Jan 2021', 'Paper 3', 'Paper')
 
+# Make the plot.
 example.plot()
