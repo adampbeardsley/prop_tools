f7bff02f39a6558cb590e1a85d14f3259c761d66
Adam Beardsley <adam.p.beardsley@gmail.com>
2019-03-31 13:51:56 -0700

    more tuning of cssi timeline

--- timeline/cssi_timeline.py
+++ timeline/cssi_timeline.py
@@ -4,7 +4,7 @@ cssi = timeline.Timeline(begin='15 Sep 2019', end='15 Oct 2022')
 
 # Create tasks with start date, end date, description, and category.
 cssi.add_task('1 Oct 2019', '31 Dec 2020', 'Prototype Pipeline Tracker', 'Inf')
-cssi.add_task('1 July 2020', '31 Dec 2021', 'SimpleDS on HERA and MWA data', 'Inf')
+cssi.add_task('1 July 2020', '31 Dec 2021', 'SimpleDS on HERA and MWA data', 'Sci')
 cssi.add_task('1 Jan 2021', '30 Sept 2021', 'Gather Costing Data', 'Inf')
 cssi.add_task('1 Oct 2021', '30 Sep 2022', 'Create Estimator Tool', 'Inf')
 
@@ -19,8 +19,15 @@ cssi.add_event('1 April 2021', 'Release V1.0', 'Inf')
 cssi.add_event('1 June 2021', 'Publish JOSS Paper', 'Comm')
 cssi.add_event('5 Jan 2022', 'AAS Workshop', 'Comm')
 cssi.add_event('1 June 2020', 'User Meeting', 'Comm')
-cssi.add_event('1 April 2022', 'SimpleDS Results', 'Inf')
+cssi.add_event('1 April 2022', 'SimpleDS Results', 'Sci')
 cssi.add_event('1 July 2022', 'Two External Adopters', 'Comm')
+
+# Jigger the colors
+cssi.categories['Inf'].color = '#d9ead3ff'  # green
+cssi.categories['Comm'].color = '#c9daf8ff'  # blue
+cssi.categories['Sci'].color = '#f4ccccff'  # red
+
+# Jigger shapes
 
 # Make the plot.
 cssi.plot()
